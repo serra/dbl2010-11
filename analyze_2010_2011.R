ec34f082b65bd659fac5222cf5fae2235110d78b
Marijn van der Zee <marijn@serraict.com>
2012-10-07 22:16:13 +0200

    Add 2011-12 file
    Date string formats do not match

--- analyze_2010_2011.R
+++ analyze_2010_2011.R
@@ -1,10 +1,14 @@
-fileName <- "./input/heren_2010_2011.csv"
-season <- substr(fileName,9,23)
+inputFiles <- list.files("./input/", full.names="true")
 
-CreateAdvancedStatsFiles(fileName)
-
-regseas <- read.csv2(sprintf("./output/%s_regseas_advanced_team_stats.csv", season))
-PrintTeamRatings(regseas, sprintf("./output/%s_regseas_advanced_teamRatings.pdf", season))
-
-playoffs <- read.csv2(sprintf("./output/%s_playoffs_advanced_team_stats.csv", season))
-PrintTeamRatings(playoffs, sprintf("./output/%s_playoffs_advanced_teamRatings.pdf", season))
+for(fileName in inputFiles) {
+  message("processing ", fileName, " ...")
+  season <- substr(fileName,9,23)
+  
+  CreateAdvancedStatsFiles(fileName)
+  
+  regseas <- read.csv2(sprintf("./output/%s_regseas_advanced_team_stats.csv", season))
+  playoffs <- read.csv2(sprintf("./output/%s_playoffs_advanced_team_stats.csv", season))
+  
+  #PrintTeamRatings(regseas, sprintf("./output/%s_regseas_advanced_teamRatings.pdf", season))
+  #PrintTeamRatings(playoffs, sprintf("./output/%s_playoffs_advanced_teamRatings.pdf", season))
+}
