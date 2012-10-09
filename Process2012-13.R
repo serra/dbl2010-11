a66daf0dbb3f5ed0b675fe67359de87573d89717
Marijn van der Zee <marijn@serraict.com>
2012-10-09 20:41:23 +0200

    Script for processing the current season

---
+++ Process2012-13.R
@@ -0,0 +1,24 @@
+# Processes all files in ./input/ and outputs csv files with advanced stats
+#
+# Optionally print additional team ratings report, buth this is commented out,
+# because it take too much time.
+
+source("CaluclateAdvancedGameStats.r")
+source("ReportTeamRatings.r")
+source("download.r")
+
+
+season <- "2012-2013"
+
+message("downloading ", season, " ...")
+fileName <- downloadseason(season)
+
+message("saved as ", fileName)
+  
+CreateAdvancedStatsFiles(fileName)
+
+regseas <- read.csv2(sprintf("./output/%s_regseas_advanced_team_stats.csv", season))
+playoffs <- read.csv2(sprintf("./output/%s_playoffs_advanced_team_stats.csv", season))
+
+#PrintTeamRatings(regseas, sprintf("./output/%s_regseas_advanced_teamRatings.pdf", season))
+#PrintTeamRatings(playoffs, sprintf("./output/%s_playoffs_advanced_teamRatings.pdf", season))
